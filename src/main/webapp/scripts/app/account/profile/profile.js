6d14a90335f68e32837bd19993e98102025e88c2
Chris Anatalio <anataliocs@gmail.com>
2015-12-01 18:41:45 -0500

    Create profile page

---
+++ src/main/webapp/scripts/app/account/profile/profile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('swankApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('profile', {
+                parent: 'account',
+                url: '/profile',
+                data: {
+                    roles: ['ROLE_USER'],
+                    pageTitle: 'Profile'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/account/profile/profile.html',
+                        controller: 'ProfileController'
+                    }
+                },
+                resolve: {
+
+                }
+            });
+    });
